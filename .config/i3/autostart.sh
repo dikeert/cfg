a0e56451cbfc667d03a7b53fb26b2abd2cf11709
Anton Ermolenko <aner@fastmail.com>
2017-11-03 09:13:32 +1100

    Moved compton to essentials
    Previously compton was in autostart
    but it actually essential for my config and I need
    to be able to change it's configuration and restart
    together with i3, therefore I'm moving it into
    essentials

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-compton -b --config ~/.config/compton.conf
 st -n MUHTERM &
 dropbox start
 telegram &
--- .config/i3/essentials.sh
+++ .config/i3/essentials.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+killall -q compton; compton -b --config ~/.config/compton.conf
 killall polybar; polybar i3 &
 feh --bg-scale ~/.config/i3/wall.*
 setxkbmap us,ru -option "grp:alt_space_toggle"
