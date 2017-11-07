5f4394a7abdb895f0e546a3c28874d47e3fb5495
Anton Ermolenko <aner@fastmail.com>
2017-11-07 21:34:43 +1100

    Changed a bit how wallpapers work

--- .config/i3/essentials.sh
+++ .config/i3/essentials.sh
@@ -2,5 +2,5 @@
 
 killall -q compton; compton -b --config ~/.config/compton.conf
 killall polybar; polybar i3 &
-feh --bg-scale ~/.config/i3/wall.*
+feh --bg-scale ~/.config/i3/bg
 setxkbmap us,ru -option "grp:alt_space_toggle"
