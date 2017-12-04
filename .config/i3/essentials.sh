3905ba9ddd5a0770c58715d2eff2a5698bc3f771
Anton Ermolenko <aner@fastmail.com>
2017-12-04 11:52:38 +1100

    TrackPoint/TrackPad settings

--- .config/i3/essentials.sh
+++ .config/i3/essentials.sh
@@ -3,4 +3,9 @@
 killall -q compton; compton -b --config ~/.config/compton.conf
 killall polybar; polybar i3 &
 feh --bg-scale ~/.config/i3/bg
+
+# input settings
+xinput set-prop 15 310 0, 1 # different time of acceleration for track point
+xinput set-prop 15 295 1 # acceleration value for track point
+xinput set-prop 14 291 1 # enable natural scrolling for trackpad
 setxkbmap us,ru -option "grp:alt_space_toggle"
